cd0850c5669af5e7b058e3f2972e0dbb346a7970
PavelKeuzerov <pit23111987@gmail.com>
2023-12-27 23:27:55 +0300

    add CRUD and view

--- AskIt/config/routes.rb
+++ AskIt/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/questions', to: 'questions#index'
+  resources :questions
 
   root 'pages#index'
 end
